9513b742a8ac3fdf652954eaf78198ed6fcba37c
Taseng Mancheykhun <taseng9@gmail.com>
2024-12-24 23:33:35 +0530

    fixed array stack

--- 6_data_structures_and_algorithm/4_stack/array_implementation.c
+++ 6_data_structures_and_algorithm/4_stack/array_implementation.c
@@ -1,48 +1,50 @@
 #include<stdio.h>
-#define MAX 10
+#define MAX 5
 
 int top=-1;
 
 void push(int b[],int num){
     if (top == MAX-1){
-        printf("Stack is Full....");
+        printf("Stack is Full\n");
     }
     else{
-    top++;
-    b[top]=num;
+        top++;
+        b[top]=num;
     };
 }
+
 void pop(int b[]){
     if (top ==-1){
-        printf("Stack is Empty..");
+        printf("Stack is Empty\n");
     }
-    else{
-    
-    b[top]=0;
-    top--;
+    else{    
+        b[top]=0;
+        top--;
     };
 }
+
+void display(int b[]){
+    for(int i=0; i<MAX; i++){
+      printf("%d ",b[i]);
+    }
+    printf("\n");
+}
+
 int main(){
   int a[MAX];
   int t;
   push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  push(a,10);
-  //pop(a);
+  push(a,12);
+  push(a,14);
+  push(a,16);
+  push(a,18);
+  push(a,20);
 
-  for(int i =0;i<MAX;i++){
-    printf("%d\t",a[i]);
-  }
-  
+  display(a);
 
-  
+  pop(a);
+
+  display(a);  
+
   return 0;
 }
